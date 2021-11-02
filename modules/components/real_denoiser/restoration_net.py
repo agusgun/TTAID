39d906b774c125b377d8cd8eef40b84f0e97dcfe
Agus Gunawan <agusgunw19@gmail.com>
2021-11-02 22:57:12 +0900

    change activation function to relu, add some ff module

--- modules/components/real_denoiser/restoration_net.py
+++ modules/components/real_denoiser/restoration_net.py
@@ -8,7 +8,7 @@ class RestorationNet(nn.Module):
     def __init__(self, input_nc, output_nc, base_features, scale_features, kernel_size, bias=False):
         super(RestorationNet, self).__init__()
 
-        act = nn.PReLU()
+        act = nn.ReLU()
 
         # Network Body
         self.shallow_fe = nn.Sequential(
@@ -32,23 +32,57 @@ class RestorationNet(nn.Module):
             conv(base_features, output_nc, kernel_size, bias=bias)
         )
 
-    def forward(self, x):
-        feat = self.shallow_fe(x)
-        feat = self.encoder(feat)
-        feat = self.decoder(feat)
-        
-        feat = feat[0]
-        residual = self.primary_head(feat)
-        clean_pred = residual + x
+    def forward(self, x, weights=None):
+        if weights is None:
+            feat = self.shallow_fe(x)
+            feat = self.encoder(feat)
+            feat = self.decoder(feat)
+            
+            feat = feat[0]
+            residual = self.primary_head(feat)
+            clean_pred = residual + x
 
-        reconstructed_noisy = self.auxiliary_head(torch.cat([clean_pred, residual], dim=1))
+            reconstructed_noisy = self.auxiliary_head(torch.cat([clean_pred, residual], dim=1))
+        else:
+            pass
 
         return clean_pred, reconstructed_noisy
 
+def primary_head_ff(x, weights):
+    ## ResidualBlock
+    net = F.conv2d(x, weights['primary_head.0.model.0.weight'], padding=1)
+    net = F.relu(net)
+    net = F.conv2d(net, weights['primary_head.0.model.2.weight'], padding=1)
+    net = net + x
+    ##
+    x = net
+    ## ResidualBlock
+    net = F.conv2d(x, weights['primary_head.1.model.0.weight'], padding=1)
+    net = F.relu(net)
+    net = F.conv2d(net, weights['primary_head.1.model.2.weight'], padding=1)
+    net = net + x
+    ##
+    net = F.conv2d(net, weights['primary_head.2.weight'], padding=1)
+    return net
+
+def auxiliary_head_ff(x, weights):
+    x = F.conv2d(x, weights['auxiliary_head.0.weight'], padding=1)
+    ## ResidualBlock
+    net = F.conv2d(x, weights['auxiliary_head.1.model.0.weight'], padding=1)
+    net = F.relu(net)
+    net = F.conv2d(net, weights['auxiliary_head.1.model.2.weight'], padding=1)
+    net = net + x
+    ##
+    net = F.conv2d(net, weights['auxiliary_head.2.weight'], padding=1)
+    return net
 
 if __name__ == "__main__":
     net = RestorationNet(3, 3, 32, 16, kernel_size=3, bias=False).cuda()
     print(sum(p.numel() for p in net.parameters() if p.requires_grad))
     x = torch.randn(4, 3, 256, 256).cuda()
     out = net(x)
-    print(out.size())
+    # print(out.size())
+    print(net)
+    for name, param in net.named_parameters():
+        print(name)
+    
